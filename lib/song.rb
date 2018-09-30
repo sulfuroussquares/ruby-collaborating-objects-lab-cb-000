8c02760c18cb655a370a005abe22f2af7f5ada1e
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-09-30 18:53:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ end
 def self.new_by_filename(file)
   name = file.split("-")[1].lstrip.rstrip
   song = Song.new(name)
-  artist_name = filename.split("-")[0].rstrip
+  artist_name = file.split("-")[0].rstrip
   song.artist = Artist.find_or_create_by_name(artist_name)
   song.artist.add_song(song)
   song
