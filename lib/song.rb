e8919f30465aa806695a2be329848e3807bf099e
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-09-30 19:05:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ def self.new_by_filename(file)
   song.artist = Artist.find_or_create_by_name(artistname)
   song.artist.add_song(song)
   song
-  puts song.artist
+  puts song.artist.name
 end
 
 end
